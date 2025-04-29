2ebacd2f0250b39c822ef7d8282c07a394507060
renben-dev <bidone.renzo@gmail.com>
2025-04-29 14:08:48 -0400

    rolling update

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -53,12 +53,66 @@ const ConferenceEvent = () => {
     };
     const getItemsFromTotalCost = () => {
         const items = [];
+        items.push(...venueItems.filter(item => item.quantity > 0).map(item => ({ ...item, type: "venue" })));
+        addonItems.forEach((item) => {
+            if (
+              item.quantity > 0 &&
+              !items.some((i) => i.name === item.name && i.type === "addon")
+            ) {
+              items.push({ ...item, type: "addon" });
+            }
+        });
+        mealsItems.forEach((item) => {
+            if (item.selected) {
+              const itemForDisplay = { ...item, type: "meals" };
+              if (item.numberOfPeople) {
+                itemForDisplay.numberOfPeople = numberOfPeople;
+              }
+              items.push(itemForDisplay);
+            }
+        });
+        return items;
+          
+
+
     };
 
     const items = getItemsFromTotalCost();
 
     const ItemsDisplay = ({ items }) => {
-
+        console.log(items);
+        return <>
+            <div className="display_box1">
+                {items.length === 0 && <p>No items selected</p>}
+                <table className="table_item_data">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Unit Cost</th>
+                            <th>Quantity</th>
+                            <th>Subtotal</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {items.map((item, index) => (
+                            <tr key={index}>
+                                <td>{item.name}</td>
+                                <td>${item.cost}</td>
+                                <td>
+                                    {item.type === "meals" || item.numberOfPeople
+                                    ? ` For ${numberOfPeople} people`
+                                    : item.quantity}
+                                </td>
+                                <td>{item.type === "meals" || item.numberOfPeople
+                                    ? `${item.cost * numberOfPeople}`
+                                    : `${item.cost * item.quantity}`}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
     };
     const calculateTotalCost = (section) => {
         let totalCost = 0;
@@ -79,11 +133,17 @@ const ConferenceEvent = () => {
 
     const navigateToProducts = (idType) => {
         if (idType == '#venue' || idType == '#addons' || idType == '#meals') {
-          if (showItems) { // Check if showItems is false
+            if (showItems) { // Check if showItems is false
             setShowItems(!showItems); // Toggle showItems to true only if it's currently false
-          }
+            }
         }
-      }
+    }
+
+    const totalCosts = {
+        venue: venueTotalCost,
+        addons: avTotalCost,
+        meals: mealsTotalCost,
+    };
 
     return (
         <>
@@ -241,7 +301,7 @@ const ConferenceEvent = () => {
                         </div>
                     ) : (
                         <div className="total_amount_detail">
-                            <TotalCost totalCosts={avTotalCost} handleClick={handleToggleItems} ItemsDisplay={() => <ItemsDisplay items={items} />} />
+                            <TotalCost totalCosts={ totalCosts } ItemsDisplay={() => <ItemsDisplay items={ items } />} />
                         </div>
                     )
                 }
